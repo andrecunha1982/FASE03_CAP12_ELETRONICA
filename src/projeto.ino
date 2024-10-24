4b6f9206287b792452435b16e68fb0b5482bbb20
andrecunha1982 <andre.cunha@outlook.com.br>
2024-10-23 22:49:03 -0300

    Ajustando arquivos para o formato de entrega solicitado

--- src/projeto.ino
+++ src/projeto.ino
@@ -1,5 +1,7 @@
 #include <DHTesp.h> // Incluindo a Biblioteca do ESP32 para Controlar o sensor de Temperatura e Humidade
 #include <LiquidCrystal_I2C.h> // Incluindo Biblioteca para Incluir um LCD no Projeto
+#include <WiFi.h>
+
 #define I2C_ADDR    0x27 // Definindo o endereço para a tela de LCD
 #define LCD_COLUMNS 20 // Quantidade de Colunas do LCD
 #define LCD_LINES   4 // Quantidade de Linhas do LCD
@@ -10,34 +12,37 @@ int Echo_pin = 18; // Definindo pino ECHO do Sensor HC-SR04
 int IRSensor = 27; // Definindo pino Sensor PIR
 int LDRSensor = 33; // Definindo pino Sensor LDR
 
-long duration;
-float Speed_of_sound =0.034;
-float dist_in_cm;
+long duration; // Definindo tipo para duração (variavel para calculo da distancia)
+float Speed_of_sound =0.034; // Definindo tipo e valor da velocidade do som (variavel para calculo da distancia)
+float dist_in_cm; // Definindo tipo da variavel final da distancia calculada 
 
 DHTesp dhtSensor; // Definindo objeto para o Sensor DHT22
 
-LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
+LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES); // Configuração do LCD do Diagrama
 
-void setup() {
+void setup() { // Setup inicial da placa ativando sensores, wifi, entre outros
 
   Serial.begin(115200);
+  WiFi.begin("Wokwi-GUEST", "", 6);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println(" Conectado! IP: " + WiFi.localIP());
   dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
   lcd.init();
   lcd.backlight();
   pinMode(Trig_pin, OUTPUT);
   pinMode(Echo_pin,INPUT);
-  Serial.println("Serial Working:");
   pinMode(IRSensor, INPUT);
-  Serial.println("LDR readings...");
   pinMode(LDRSensor,INPUT);
 }
 
-void loop() {
-  //Sensor DHT22 de Temperatura e Humidade
+void loop() { // Configuração do Loop de informações para monitorar constantemente o resultado dos sensores e jogar informações para o LCD do projeto
+  // Valores do Sensor DHT22 de Temperatura e Humidade
   TempAndHumidity  data = dhtSensor.getTempAndHumidity();
-  Serial.println("Temperatura: " + String(data.temperature, 1) + "°C" + " | " + "Humidade: " + String(data.humidity, 1) + "%");
 
-  // Sensor HC-SR04 de Distancia
+  // Valores do Sensor HC-SR04 de Distancia
   digitalWrite(Trig_pin, LOW);
   delayMicroseconds(2);
   digitalWrite(Trig_pin, HIGH);
@@ -45,21 +50,19 @@ void loop() {
   digitalWrite(Trig_pin,LOW);
   duration =pulseIn(Echo_pin,HIGH);
   dist_in_cm=duration*Speed_of_sound/2;
-  Serial.print("Distancia em cm: " + String(dist_in_cm)+"\n");
 
-  //Sensor LDR de Luminosidade
+  // Valores do Sensor LDR de Luminosidade
   int a=analogRead(LDRSensor);
-  Serial.println("Luminosidade: " + String(a) + " LUX");
 
-  //Sensor PIR para Detecção de Movimento
+  //Valores do Sensor PIR para Detecção de Movimento e unificação de valores em um vetor
   int IR1 = digitalRead(IRSensor);
   if (IR1 == 0)
   {
-    Serial.println("Movimento Detectado!");
+    Serial.println("["+String(data.temperature,1)+","+String(data.humidity,1)+","+String(dist_in_cm)+","+String(a)+","+"1"+"]");
   }
   else
   {
-    Serial.println("Movimento Encerrado!");
+    Serial.println("["+String(data.temperature,1)+","+String(data.humidity,1)+","+String(dist_in_cm)+","+String(a)+","+"0"+"]");
   }
   Serial.println("---");
 
@@ -82,5 +85,5 @@ void loop() {
   }
 
 
-  delay(500);
+  delay(250);
 }
